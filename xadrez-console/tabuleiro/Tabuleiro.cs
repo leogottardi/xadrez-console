1ef13a35320cff56a90bb4234337b844ea498183
Leonardo Moitinho Gottardi <leonardo.gottardi@uniodigital.com.br>
2020-09-10 08:32:56 -0300

    comecando a colocar pecas no tabuleiro

--- xadrez-console/tabuleiro/Tabuleiro.cs
+++ xadrez-console/tabuleiro/Tabuleiro.cs
@@ -1,5 +1,4 @@
-﻿using xadrez_console.tabuleiro;
-
+﻿
 namespace tabuleiro
 {
     class Tabuleiro
@@ -17,6 +16,12 @@ namespace tabuleiro
         public Peca peca(int linha, int coluna)
         {
             return pecas[linha, coluna];
+        }
+
+        public void colocarPeca(Peca p, Posicao pos)
+        {
+            pecas[pos.linha, pos.coluna] = p;
+            p.posicao = pos;
         }
     }
 }
